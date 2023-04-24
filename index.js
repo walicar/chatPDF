93d13c9552d336c2b1a2f3d236b5ec2455a6ad0a
wvacalm <w.alicarjr@gmail.com>
2023-04-24 11:54:47 -0700

    reformat index.js, fix redirects to accomodate new upload page

--- index.js
+++ index.js
@@ -1,129 +1,141 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import fs from 'fs';
-import multer from 'multer';
-import bodyParser from 'body-parser';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import fs from "fs";
+import multer from "multer";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath, parse } from "url";
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+// const io = new Server(server);
 const upload = multer({ dest: "uploads/" });
 // store me somewhere else, export me yknow, put me into an object
 let state = {
-  status: '',
-  currentFile: '', // will always contain path of current file
-  error: '',
-  response: '',
-  index: '',
+  status: "",
+  currentFile: "", // will always contain path of current file
+  error: "",
+  response: "",
+  index: "",
   indices: [],
-  vectorStore: ''
-}
+  vectorStore: "",
+};
 // idk
-import { getTexts, createEmbeddings, queryDoc, getIndices, getPineconeStore, mockPromiseFail, checkIndex, mockPromisePass} from './util.js'
+import {
+  checkIndex,
+  createEmbeddings,
+  getIndices,
+  getPineconeStore,
+  getTexts,
+  mockPromiseFail,
+  mockPromisePass,
+  queryDoc,
+} from "./util.js";
 // globals to be used by server only
-let texts = '';
+let texts = "";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.post('/pdfupload', upload.single("doc"), async (req, res) => {
-  const file = req.file; 
+app.post("/pdfupload", upload.single("doc"), async (req, res) => {
+  const file = req.file;
   try {
     fs.readFile(file.path, async (err, data) => {
-    if (err) throw err;
-    fs.writeFile(`uploads/${file.originalname}`, data, async (err) => {
       if (err) throw err;
-      console.log(`File uploaded: ${file.originalname}`);
-      fs.unlink(file.path, async (err) => {
+      fs.writeFile(`uploads/${file.originalname}`, data, async (err) => {
         if (err) throw err;
-        state.currentFile = `./uploads/${file.originalname}`;
-        try {  
-          texts = await getTexts(`./uploads/${file.originalname}`)
-          state.status = `PDF Loaded Successfully`;
-          console.log(`File processed, text length: ${texts.length}`)
-        } catch (e) {console.log(e)}
-        res.redirect('/')
+        console.log(`File uploaded: ${file.originalname}`);
+        fs.unlink(file.path, async (err) => {
+          if (err) throw err;
+          state.currentFile = `./uploads/${file.originalname}`;
+          try {
+            texts = await getTexts(`./uploads/${file.originalname}`);
+            state.status = `PDF Loaded Successfully`;
+            console.log(`File processed, text length: ${texts.length}`);
+          } catch (e) {
+            console.log(e);
+          }
+          res.redirect("/upload");
+        });
       });
     });
-  });
   } catch (e) {
-    console.log(e)
-    state.error = 'Could not upload PDF'
+    console.log(e);
+    state.error = "Could not upload PDF";
   }
 });
 
-app.post('/makeEmbeddings', async (req, res) => {
+app.post("/makeEmbeddings", async (_req, res) => {
   if (texts && state.index) {
-    console.log('trying to create embeddings')
+    console.log("trying to create embeddings");
     try {
       // DUMMY CODE
       state.vectorStore = await createEmbeddings(texts, state.index);
       // await mockPromisePass();
-      console.log('Embeddings Fulfilled, vectorStore set.');
-      state.status = 'Created embeddings!';
+      console.log("Embeddings Fulfilled, vectorStore set.");
+      state.status = "Created embeddings!";
     } catch (e) {
       state.error = e;
       console.log(e);
     }
-  } else { state.error = 'no texts or no selected index.'; }
-  res.redirect('/')
-})
+  } else state.error = "no texts or no selected index.";
+  res.redirect("/upload");
+});
 
-app.post('/query', async (req, res) => {
+app.post("/query", async (req, res) => {
   console.log(`Query to be sent: ${req.body.query}`);
   if (state.vectorStore && req.body.query) {
     try {
       // DUMMY CODE
-      state.response = await queryDoc(req.body.query, state.vectorStore)
+      state.response = await queryDoc(req.body.query, state.vectorStore);
       // state.response = await mockPromisePass();
-      console.log('Query Fulfilled');
+      console.log("Query Fulfilled");
     } catch (e) {
       state.error = e;
       console.log(e);
     }
-  } else { state.error = 'Error sending query'; }
-  res.redirect('/');
-})
+  } else state.error = "Error sending query";
+  res.redirect("/home");
+});
 
-app.post('/getIndices', async (req, res) => {
-  console.log('Attempting to get indices')
+app.post("/getIndices", async (req, res) => {
+  console.log("Attempting to get indices");
+  const redirectURL = parse(req.get("Referer")).pathname;
   try {
     state.indices = await getIndices();
     console.log(state.indices);
-  } catch (e) { 
-    console.log(e) 
+  } catch (e) {
+    console.log(e);
     state.error = e;
   }
-  res.redirect('/');
-})
-
-app.post('/setIndex', async (req, res) => {
-  state.index = req.body.index;
-  console.log(typeof state.vectorStore)
-  state.vectorStore = await getPineconeStore(state.index);
-  console.log(typeof state.vectorStore)
-  console.log(state.vectorStore);
-  console.log(`state.index set to: ${req.body.index}, state.vectorStore is set`)
-  const desc = await checkIndex({ indexName: state.index });
-  console.log(desc);
-  res.redirect('/');
-})
-
-app.get('/', (_req, res) => {
-  res.render('index', state)
+  res.redirect(redirectURL);
 });
 
-app.get('/home', (_req, res) => {
-  res.render('home', state);
-})
+app.post("/setIndex", async (req, res) => {
+  const redirectURL = parse(req.get("Referer")).pathname;
+  state.index = req.body.index;
+  state.vectorStore = await getPineconeStore(state.index);
+  console.log(
+    `state.index set to: ${req.body.index}, state.vectorStore is set`,
+  );
+  const desc = await checkIndex({ indexName: state.index });
+  console.log(desc);
+  res.redirect(redirectURL);
+});
 
-app.get('/upload', (_req, res) => {
-  res.render('upload', state);
-})
+app.get("/", (_req, res) => {
+  res.render("index", state);
+});
+
+app.get("/home", (_req, res) => {
+  res.render("home", state);
+});
+
+app.get("/upload", (_req, res) => {
+  res.render("upload", state);
+});
 
 /*
 io.on('connection', (socket) => {
@@ -132,7 +144,7 @@ io.on('connection', (socket) => {
 */
 
 server.listen(3000, () => {
-  console.log('listening on http://localhost:3000/');
-  console.log('visit the new page at http://localhost:3000/home')
-  console.log('visit the upload page at http://localhost:3000/upload')
+  console.log("listening on http://localhost:3000/");
+  console.log("visit the new page at http://localhost:3000/home");
+  console.log("visit the upload page at http://localhost:3000/upload");
 });
