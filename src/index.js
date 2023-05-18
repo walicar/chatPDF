a156877f838012d9c004cd56aa50794af84c8d7b
wvacalm <w.alicarjr@gmail.com>
2023-05-18 14:52:32 -0700

    remove unecessary code

--- src/index.js
+++ src/index.js
@@ -30,7 +30,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
-console.log(__dirname);
 
 app.post("/query", async (req, res) => {
   if (state.vectorStore && req.body.query) {
