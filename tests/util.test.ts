ab3131a001a3278d23a7cdc44397d9ef9fa64a88
wvacalm <w.alicarjr@gmail.com>
2023-05-25 11:46:21 -0700

    tests work, configured for ECMAScript Modules

--- tests/util.test.ts
+++ tests/util.test.ts
@@ -1,17 +1,27 @@
-import util from "../src/lib/util";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 // all tests are run in root dir
 
 /*
 test("getIndices does not throw", async () => {
   expect(async () => {await util.getIndices()}).not.toThrow();
 })
-
-test("getTexts does not fail", async () => {
-  const result = await util.getTexts("tests/test.pdf");
-  expect(result.length).toBe(17);
-})
 */
 
+test("getTexts does not fail", async () => {
+  const loader = new PDFLoader("./tests/test.pdf");
+  const doc = await loader.load();
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 20,
+  });
+  const docs = await splitter.splitDocuments(doc);
+  console.log("HELLO?");
+  console.log(docs.length);
+  expect(docs.length).toBe(17);
+})
+
+
 test("langchain segfaults jest", async () => {
-  expect(17).toBe(17);
+  expect(14).toBe(17);
 })
