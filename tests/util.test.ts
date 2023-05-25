91706110040b27b967637e224f2fb213bc413980
wvacalm <w.alicarjr@gmail.com>
2023-05-24 23:43:05 -0700

    convert test file to ts

---
+++ tests/util.test.ts
@@ -0,0 +1,14 @@
+import util from "../src/util";
+// all tests are run in root dir
+
+/*
+test("getIndices does not throw", async () => {
+  expect(async () => {await util.getIndices()}).not.toThrow();
+})
+*/
+
+test("getTexts does not fail", async () => {
+  const result = await util.getTexts("tests/test.pdf");
+  expect(result.length).toBe(17);
+})
+
