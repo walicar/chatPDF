d9f6c039f5feacf2947c782a7f935ec2862875db
wvacalm <w.alicarjr@gmail.com>
2023-05-25 17:06:05 -0700

    base functionality for Chroma

--- src/lib/chromaHelper.js
+++ src/lib/chromaHelper.js
@@ -2,28 +2,34 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import { Chroma } from "langchain/vectorstores/chroma";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { ChromaClient } from "chromadb";
+import { ChromaClient, OpenAIEmbeddingFunction} from "chromadb";
+import { OpenAI } from "langchain/llms/openai";
+import { loadQAStuffChain } from "langchain/chains";
 export class ChromaHelper {
   constructor() {
     this.currentStore = undefined;
+  }
+  async createEmbeddingsV2(texts, name) {
+    const client = new ChromaClient();
+    const embedder = new OpenAIEmbeddingFunction({openai_api_key: process.env.OPENAI_API_KEY});
+    const collection = await client.createCollection({name, embeddingFunction: embedder});
+    this.currentStore = collection;
   }
 
   async createEmbeddings(docs, name) {
     const embeddings = new OpenAIEmbeddings({
       openAIApiKey: process.env.OPENAI_API_KEY,
     });
-    const metadatas = [{}];
-    const store = await Chroma.fromTexts(texts, metadatas, embeddings, {
-      collectionName: name,
-    });
-    this.store = store;
-  }
-
-  async createCollection(name) {
     const client = new ChromaClient();
+    const metadatas = [{}];
     try {
-      const res = await client.createCollection({ name });
-      return res;
+      const embedder = new OpenAIEmbeddingFunction({openai_api_key: process.env.OPENAI_API_KEY});
+      await client.createCollection({name, embeddingFunction: embedder});
+      const store = await Chroma.fromTexts(docs, metadatas, embeddings, {
+      collectionName: name,
+      });
+      this.store = store;
+      return store;
     } catch (e) {
       return e;
     }
@@ -36,6 +42,21 @@ export class ChromaHelper {
       return res;
     } catch (e) {
       return e;
+    }
+  }
+
+  async queryDoc(query) {
+    if (this.store) {
+      const docs = await this.store.similaritySearch(query, 1);
+      const llm = new OpenAI({
+        openAIApiKey: process.env.OPENAI_API_KEY,
+        temperature: 0.3
+      })
+      const chain = loadQAStuffChain(llm);
+      const answer = await chain.call({input_documents: docs, question: query});
+      return answer.text;
+    } else {
+      return Error("Store does not exist");
     }
   }
 
--- tests/chroma.test.js
+++ tests/chroma.test.js
@@ -1,8 +1,16 @@
 import { ChromaHelper } from "../src/lib/chromaHelper";
 import { expect } from "@jest/globals";
-test("create collection", async () => { 
+import { util } from "../src/lib/util.js";
+
+test("create embeddings with collection", async () => {
   const helper = new ChromaHelper();
-  const create = await helper.createCollection("test");
-  await helper.deleteCollection("test");
-  expect(create).not.toContain("Error");
-});
+  const name = "exist";
+  const texts = await util.getTexts("./tests/camby.pdf");
+  await helper.deleteCollection(name)
+  const store = await helper.createEmbeddings(texts, name);
+  const result = await helper.queryDoc("What is life?");
+  console.log(result);
+  expect(result).toBe("OK!");;
+  await helper.deleteCollection(name);
+}, 15000)
+
