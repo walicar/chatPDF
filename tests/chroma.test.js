3c7cd93a74ef3796b00971f153adba8d7ba7dbf3
wvacalm <w.alicarjr@gmail.com>
2023-05-25 18:58:54 -0700

    update tests

--- tests/chroma.test.js
+++ tests/chroma.test.js
@@ -2,13 +2,13 @@ import { ChromaHelper } from "../src/lib/chromaHelper";
 import { expect } from "@jest/globals";
 import { util } from "../src/lib/util.js";
 
-test("create embeddings with collection", async () => {
+test.skip("create embeddings with collection", async () => {
   const helper = new ChromaHelper();
   const name = "exist";
-  const texts = await util.getTexts("./tests/camby.pdf");
+  const texts = await util.getTexts("./tests/test.pdf");
   await helper.deleteCollection(name);
   const store = await helper.createEmbeddings(texts, name);
-  const result = await helper.queryDoc("What is life?");
+  const result = await helper.queryDoc("What is openAI?");
   console.log(result);
   expect(result).toBe("OK!");
   await helper.deleteCollection(name);
