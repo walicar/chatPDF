5226768a5d4f04240fc89cf4ec041e54eac85fd3
wvacalm <w.alicarjr@gmail.com>
2023-05-25 21:07:48 -0700

    update test questions

--- tests/chroma.test.js
+++ tests/chroma.test.js
@@ -8,8 +8,8 @@ test.skip("create embeddings with collection", async () => {
   const texts = await util.getTexts("./tests/test.pdf");
   await helper.deleteCollection(name);
   const store = await helper.createEmbeddings(texts, name);
-  const result = await helper.queryDoc("What is openAI?");
+  const result = await helper.queryDoc("What is OpenAI?");
   console.log(result);
-  expect(result).toBe("OK!");
+  expect(result).toContain("OpenAI");
   await helper.deleteCollection(name);
 }, 15000);
