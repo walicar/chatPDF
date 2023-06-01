b1e9b2504e8dfa70f61ab44fadd59ec2c8ac5163
wvacalm <w.alicarjr@gmail.com>
2023-05-31 17:03:00 -0700

    fix chroma tests

--- tests/chroma.test.js
+++ tests/chroma.test.js
@@ -1,4 +1,4 @@
-import { ChromaHelper } from "../src/lib/ChromaHelper";
+import { ChromaHelper } from "../src/lib/chromaHelper";
 import { expect } from "@jest/globals";
 import { util } from "../src/lib/util.js";
 
